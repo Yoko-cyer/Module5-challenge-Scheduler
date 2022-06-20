01607e967fbad6019ba0ee7d631707296226f2b1
Yoko <yokoujihara622@gmail.com>
2022-06-20 20:30:29 +1000

    added readme

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -2,7 +2,7 @@
 
 function startClock () {
   setInterval(function(){
-    const now = moment().format("YYY-MM-DD: HH:mm:ss");
+    const now = moment().format("YY-MM-DD: HH:mm:ss");
     $("#current-time").text(now)
   }, 1000);
 };
@@ -10,34 +10,64 @@ function startClock () {
 
 function createTimeblock(hour) {
 
+  {/* <div class="row">
+        <div class="time-col col-2">9:00</div>
+        <div class="textarea-col col-8">
+          <textarea name="" id="" rows="3"></textarea>
+        </div>
+        <div class="button-col col-2">
+          <button class="btn btn-primary">Save</button>
+        </div>
+      </div> */}
   const row = $("<div>");
 
   const currentHour = Number(moment().format("H"));
 
   const isPast = hour < currentHour;
-
-  const isCuerrent = hour === currentHour;
-
-  const isFuture = hour > currentHour
   
+  const isPresent = hour === currentHour;
+  
+  const isFuture = hour > currentHour;
+  
+  let rowClass = "row";
+
+  // if timeblock is in the future -- give it a 'future' class
+  // if timeblock is in the past -- give it a 'past' class'
+  // if timeblock is in the present -- give it a 'present' class'
+  
+  if(isPast){
+    rowClass = rowClass + " past";
+  }
+  if(isPresent){
+    rowClass = rowClass + " present";
+  }
+  if(isFuture){
+    rowClass = rowClass + " future";
+  }
+
   
   row.attr("class", "row");
+
   
-  const timeCol = $("<div");
+  const timeCol = $("<div>");
   timeCol.attr("class", "time-col col-2");
+  
+  timeCol.text(hour + ":00");
 
-  timeCol.text(hour + ":00")
-
-  const textareaCol = $("<div");
+  const textareaCol = $("<div>");
   textareaCol.attr("class", "textarea-col col-8");
 
   const textarea = $('<textare rows="3">');
   textareaCol.append(textarea);
 
-  const buttonCol = $("<div");
+  // with existing details from local  storage
+  const existingNotes = localStorage.getItem(hour);
+  textarea.val(existingNotes);
+
+  const buttonCol = $("<div>");
   buttonCol.attr("class", "button-col col-2");
 
-  const saveBtn = $('<button class="btn btn-primary">');
+  const saveBtn = $('<button class="btn btn-primary save-button">');
   saveBtn.text("Save");
 
   buttonCol.append(saveBtn);
@@ -46,24 +76,7 @@ function createTimeblock(hour) {
   return row;
 
 }
-// if timeblock is in the future -- give it a 'future' class
-// if timeblock is in the past -- give it a 'past' class'
-  // if timeblock is in the present -- give it a 'present' class'
   
-  {/* <div class="row">
-          <div class="time-col col-2">9:00</div>
-          <div class="textarea-col col-8">
-            <textarea name="" id="" rows="3"></textarea>
-          </div>
-          <div class="button-col col-2">
-            <button class="btn btn-primary">Save</button>
-          </div>
-        </div> */}
-
-        // when user click on the save button of a particular time block
-        // grab the user input
-        // save to local storage
-
 
 
 // I should see a clock in the header
@@ -81,21 +94,21 @@ $(function(){
     timeBlockContainer.append(timeBlock);
     
   }
-  
-
 });
 
-
-  
-
-
-
-
-
-
 $(document).on("click", ".save-button", function(event){
+  // when user click on the save button of a particular time block
+  // grab the user input
+  // save to local storage
   const buttonClicked = $(event.target);
-
+  
   const textarea = buttonClicked.parent().prev().children();
   const timeCol = buttonClicked.parent().prev().prev();
+  const time = timeCol.text();
+  const hour = time.slice(0, -3);
+  
+  const userInput = textarea.val();
+
+  localStorage.setItem(hour, userInput);
+  
 })
